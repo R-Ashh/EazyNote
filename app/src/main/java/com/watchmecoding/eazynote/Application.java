78a2b878834aac375656b6453ce9184e9f0e5fe0
Arash Afsharpour <arash72af@gmail.Com>
2017-10-12 11:04:40 +0300

    0.2

--- app/src/main/java/com/watchmecoding/eazynote/Application.java
+++ app/src/main/java/com/watchmecoding/eazynote/Application.java
@@ -1,10 +1,8 @@
 package com.watchmecoding.eazynote;
 
 
-import android.widget.Button;
 
 public class Application extends android.app.Application {
-
     @Override
     public void onCreate() {
         super.onCreate();
