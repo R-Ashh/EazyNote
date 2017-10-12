d6302d74f7481f65f2f8174cf576ae18d3b77932
Arash Afsharpour <arash72af@gmail.Com>
2017-10-12 23:15:14 +0300

    0.6

--- app/src/main/java/com/watchmecoding/eazynote/activity/NoteEditorActivity.java
+++ app/src/main/java/com/watchmecoding/eazynote/activity/NoteEditorActivity.java
@@ -1,9 +1,7 @@
 package com.watchmecoding.eazynote.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
