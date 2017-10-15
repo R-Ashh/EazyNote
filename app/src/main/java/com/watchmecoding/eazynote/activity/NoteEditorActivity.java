397d087ae68c496c12c52850faedd479ec0d7d16
Arash Afsharpour <arash72af@gmail.Com>
2017-10-16 01:46:29 +0300

    0.8

--- app/src/main/java/com/watchmecoding/eazynote/activity/NoteEditorActivity.java
+++ app/src/main/java/com/watchmecoding/eazynote/activity/NoteEditorActivity.java
@@ -1,13 +1,16 @@
 package com.watchmecoding.eazynote.activity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.watchmecoding.eazynote.R;
@@ -18,14 +21,17 @@ import com.watchmecoding.eazynote.data.NoteItem;
 public class NoteEditorActivity extends AppCompatActivity implements View.OnClickListener {
 
     private NoteItem note;
+    private NoteDataSource datasource;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_editor);
 
-        Button bt = (Button) findViewById(R.id.save_button);
-        bt.setOnClickListener(this);
+        Button savebtn = (Button) findViewById(R.id.save_button);
+        savebtn.setOnClickListener(this);
+        Button deleteButton = (Button) findViewById(R.id.delete_button);
+        deleteButton.setOnClickListener(this);
 
         Toolbar myToolbar = (Toolbar) findViewById(R.id.app_toolbar);
         setSupportActionBar(myToolbar);
@@ -39,8 +45,30 @@ public class NoteEditorActivity extends AppCompatActivity implements View.OnClic
             et.setText(note.getText());
             et.setSelection(note.getText().length());
         }
-
     }
+
+    private void deleteButtonClicked(final NoteItem item) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(NoteEditorActivity.this);
+        builder.setTitle("Are you sure you want to delete this note?");
+        builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                NoteDataSource source = new NoteDataSource(NoteEditorActivity.this);
+                source.remove(item);
+                saveAndFinish();
+            }
+        });
+
+        builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
 
     private void saveAndFinish() {
         EditText et = (EditText) findViewById(R.id.noteText);
@@ -69,12 +97,15 @@ public class NoteEditorActivity extends AppCompatActivity implements View.OnClic
         finish();
     }
 
+
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.save_button:
                 saveAndFinish();
                 break;
+            case R.id.delete_button:
+            deleteButtonClicked(note);
             default:
                 break;
         }
